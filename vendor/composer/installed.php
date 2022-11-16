a6ca3a95a742bb60fd1939d08f9221bf7d636b42
codeYi <yolo_me@163.com>
2022-11-16 18:33:43 +0800

    package name modify

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => '1',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'php/xlswriter',
+        'reference' => 'e59012bc51ad78fd9f2330920f41b78fbc9f26fb',
+        'name' => 'code/xlswriter',
         'dev' => true,
     ),
     'versions' => array(
-        'php/xlswriter' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'code/xlswriter' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => '1',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'e59012bc51ad78fd9f2330920f41b78fbc9f26fb',
             'dev_requirement' => false,
         ),
     ),
